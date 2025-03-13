788ac68ec8eca3a33c26feac057a9fd4f6b2656f
Kelven Souza <kelven.souza.2025@gmail.com>
2025-03-13 17:14:12 -0300

    Added user avatar

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,11 +1,12 @@
 import { SidebarTrigger } from "@/components/ui/sidebar"; 
+import UserMenu from "./user-menu";
 
 export default function Header() {
   return (
     <div className="sticky top-0 z-10 flex flex-col">
-      <header className="flex h-14 items-center gap-2 border-b border-foreground/10 bg-background px-4 lg:h-[60px]">
+      <header className="flex h-14 items-center gap-2 border-b border-foreground/10 bg-background px-4 lg:h-[60px] justify-between">
         <SidebarTrigger className="*:size-5" />
-        <h1>Teste</h1>
+        <UserMenu />
       </header>
     </div>
   );
