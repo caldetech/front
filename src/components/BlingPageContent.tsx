cf2689e9cb8b35752be73d10b91e0b1bc8eef6b8
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-06 12:15:24 -0300

    Under development

---
+++ src/components/BlingPageContent.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { getBlingTokens } from "@/http/get-bling-tokens";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
+
+export default function BlingPageContent() {
+  const [loading, setLoading] = useState(true);
+  const params = useSearchParams();
+  const code = params.get("code");
+  const state = params.get("state");
+  const router = useRouter();
+
+  useEffect(() => {
+    async function handleBlingTokens() {
+      if (code && state) {
+        const tokens = await getBlingTokens({ code, state });
+
+        if (tokens) {
+          router.push(`http://gestao.caldetech.com.br/${state}/integracoes`);
+        }
+      }
+    }
+
+    handleBlingTokens();
+  }, [code]);
+
+  return (
+    <div className="abosolute top-1/2 left-1/2 flex h-screen w-screen items-center justify-center">
+      {loading && (
+        <div className="flex flex-col items-center justify-center gap-8">
+          <div className="flex flex-col">
+            <h2 className="mt-6 text-3xl font-bold text-foreground">
+              Por favor, aguarde
+            </h2>
+
+            <p className="mt-2 text-sm text-foreground">
+              Estamos conectando sua conta ao Bling
+            </p>
+          </div>
+
+          <BeatLoader />
+        </div>
+      )}
+    </div>
+  );
+}
