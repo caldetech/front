60399dcf36941f2d2970d4213f240cebdf37e421
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-07 11:52:21 -0300

    fix(swr): change api client to add cookies

--- src/lib/api-client-client.ts
+++ src/lib/api-client-client.ts
@@ -1,9 +1,12 @@
 import ky from "ky";
 
-const token = localStorage.getItem("token");
+const isBrowser = typeof window !== "undefined";
+
+const token = isBrowser ? localStorage.getItem("token") : null;
 
 export const api = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_API_URL,
+  credentials: "include",
   headers: token
     ? {
         Authorization: `Bearer ${token}`,
