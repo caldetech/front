20fca820cc4e3086278963a7830128a1f0b6b32b
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-08 19:49:21 -0300

    refactor(cookies): rollback initial cookies logic

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,18 +1,15 @@
 "use server";
 
 import ky from "ky";
-import { cookies } from "next/headers"; // Importa a função cookies
+import { cookies } from "next/headers";
 
 export const api = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_API_URL,
-  credentials: "include",
   hooks: {
     beforeRequest: [
       async (request) => {
-        // Obtém o armazenamento de cookies no lado do servidor
         const cookieStore = await cookies();
 
-        // Tenta obter o cookie "token"
         const token = cookieStore.get("token");
 
         if (token) {
