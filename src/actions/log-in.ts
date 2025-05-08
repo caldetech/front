e07d5822ceb95080d4f2b72111dc2ca368f0794a
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-08 13:37:52 -0300

    fix(cookies): add front cookies

--- src/actions/log-in.ts
+++ src/actions/log-in.ts
@@ -29,35 +29,35 @@ export async function LogInAction(data: FormData) {
 
   const { email, password } = result.data;
 
-  // try {
-  //   const { token } = await LogIn({
-  //     email,
-  //     password,
-  //   });
+  try {
+    const { token } = await LogIn({
+      email,
+      password,
+    });
 
-  //   const storedCookies = await cookies();
+    const storedCookies = await cookies();
 
-  //   storedCookies.set("token", token, {
-  //     maxAge: 60 * 60 * 24 * 7, // 7 days
-  //     path: "/",
-  //   });
-  // } catch (err) {
-  //   if (err instanceof HTTPError) {
-  //     const { message } = await err.response.json();
+    storedCookies.set("token", token, {
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+      path: "/",
+    });
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json();
 
-  //     return {
-  //       success: false,
-  //       message,
-  //       errors: null,
-  //     };
-  //   }
+      return {
+        success: false,
+        message,
+        errors: null,
+      };
+    }
 
-  //   return {
-  //     success: false,
-  //     message: "Unexpected error, try again in a few minutes.",
-  //     errors: null,
-  //   };
-  // }
+    return {
+      success: false,
+      message: "Unexpected error, try again in a few minutes.",
+      errors: null,
+    };
+  }
 
   return {
     success: true,
