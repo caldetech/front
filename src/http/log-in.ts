4536c18548d41175ad573c14b4383248005155c6
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-08 17:38:56 -0300

    feat(auth): change ky cookies logic

--- src/http/log-in.ts
+++ src/http/log-in.ts
@@ -13,6 +13,7 @@ export async function LogIn({
         email,
         password,
       },
+      credentials: "include",
     })
     .json<{ message: string }>();
 }
--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -2,7 +2,4 @@ import ky from "ky";
 
 export const api = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    credentials: "include",
-  },
 });
