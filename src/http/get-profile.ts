37b8b9ed8e2a6fd58d659d6b4ee209547b2010b1
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-08 20:41:11 -0300

    refactor(requests): change requests api logic

--- src/http/get-profile.ts
+++ src/http/get-profile.ts
@@ -3,13 +3,31 @@ import { api } from "../lib/api-client";
 import type { User } from "@/utils/casl/user";
 
 export async function getProfile(slug: string) {
-  const userWithProfile = await api
-    .post("auth/profile", {
-      json: {
-        slug,
-      },
-    })
-    .json<User>();
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/auth/profile`;
 
-  return userWithProfile;
+  const requestBody = JSON.stringify({
+    slug,
+  });
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Envia as credenciais (cookies) com a requisição
+      body: requestBody,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Erro ao obter perfil");
+    }
+
+    const data = await response.json();
+    return data as User;
+  } catch (error) {
+    console.error("Erro ao obter o perfil:", error);
+    throw error;
+  }
 }
